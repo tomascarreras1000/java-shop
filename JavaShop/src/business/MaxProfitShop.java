30fe6fd253ecec0776aae9afd4363b1d9dcbc4c1
tomascarreras1000 <tomascarreras1000@gmail.com>
2024-06-11 11:22:09 +0200

    Update MaxProfitShop.java

--- JavaShop/src/business/MaxProfitShop.java
+++ JavaShop/src/business/MaxProfitShop.java
@@ -1,7 +1,10 @@
 package business;
 
 public class MaxProfitShop extends Shop{
-    public MaxProfitShop(String name, String description, int since, float earnings) {
-        super(name, description, since, earnings);
+    public MaxProfitShop(String name, String description, int since) {
+        this.name = name;
+        this.description = description;
+        this.since = since;
+        this.earnings = 0f;
     }
 }
