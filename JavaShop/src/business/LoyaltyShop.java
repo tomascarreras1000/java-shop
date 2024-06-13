90ae19a7d0f42b353aec7168f2763476c3954702
tomascarreras1000 <tomascarreras1000@gmail.com>
2024-06-13 11:54:47 +0200

    CurrentLoyalty (con setter)

--- JavaShop/src/business/LoyaltyShop.java
+++ JavaShop/src/business/LoyaltyShop.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 
 public class LoyaltyShop extends Shop {
     private float loyaltyThreshold;
+    private float currentLoyalty;
 
     public LoyaltyShop(String name, String description, int since, float loyaltyThreshold) {
         this.name = name;
@@ -11,6 +12,7 @@ public class LoyaltyShop extends Shop {
         this.since = since;
         this.earnings = 0f;
         this.loyaltyThreshold = loyaltyThreshold;
+        this.currentLoyalty = 0f;
         this.catalogue = new LinkedList<RetailProduct>();
     }
 
@@ -20,10 +22,19 @@ public class LoyaltyShop extends Shop {
         this.since = since;
         this.earnings = earnings;
         this.loyaltyThreshold = loyaltyThreshold;
+        this.currentLoyalty = 0f;
         this.catalogue = catalogue;
     }
 
     public float getLoyaltyThreshold() {
         return loyaltyThreshold;
+    }
+
+    public void setCurrentLoyalty(float newLoyalty) {
+        this.currentLoyalty = newLoyalty;
+    }
+
+    public float getCurrentLoyalty() {
+        return currentLoyalty;
     }
 }
